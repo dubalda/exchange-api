411659cff11a783ab70bbdcba9b29e9e6602d6f9
Laptev Stanislav <laptevss@gmail.com>
2021-06-01 01:57:31 +0300

    done

--- binance/python/expample.create_order.sh
+++ binance/python/expample.create_order.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# testnet.binance.vision (spot)
-#binance_api="111" \
-#  binance_secret="222" \
-#  python3 create_order.py
+# fapi.binance.com
+binance_api="75a90f1f58f" \
+  binance_secret="198a27f" \
+  binance_futures_url="https://dapi.binance.com" \
+  python3 create_order.py
 
 
 # testnet.binancefuture.com
-binance_api="111" \
-  binance_secret="222" \
-  python3 create_order.py
+#binance_api="75a90f1a5373fb58f" \
+#  binance_secret="19d02a0bbaca27f" \
+#  binance_futures_url="https://testnet.binancefuture.com" \
+#  python3 create_order.py
 
