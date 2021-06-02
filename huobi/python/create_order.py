4ebd1c8f37c69881a950ed287bd22470e41f287b
Laptev Stanislav <laptevss@gmail.com>
2021-06-02 21:04:36 +0300

    huobi python init

--- huobi/python/create_order.py
+++ huobi/python/create_order.py
@@ -1,3 +1,5 @@
 import huobi
 
 huobi.swap_openorders('btc-usd')
+
+#huobi.swap_mark_price_kline(contract_code = 'btc-usd', period = '1min', size = '1')
--- huobi/python/huobi/__init__.py
+++ huobi/python/huobi/__init__.py
@@ -7,35 +7,43 @@ from urllib.parse import urlencode
 from datetime import datetime
 import os
 
-def build_url(method: 'str', endpoint: 'str'):
-  global AccessKeyId
-  global SecretKey
-  global timestamp
-  global params
-  global base_uri
+global base_uri
+global AccessKeyId
+global SecretKey
 
-  #Get all Accounts of the Current User
-  AccessKeyId = os.environ.get('accessKey')
-  SecretKey = os.environ.get('secretKey')
-  timestamp = str(datetime.utcnow().isoformat())[0:19]
-  params = urlencode({'AccessKeyId': AccessKeyId,
-                    'SignatureMethod': 'HmacSHA256',
-                    'SignatureVersion': '2',
-                    'Timestamp': timestamp
-                   })
-  base_uri = 'api.hbdm.vn'
-  pre_signed_text = method + '\n' + base_uri + '\n' + endpoint + '\n' + params
+base_uri = 'api.hbdm.vn'
+AccessKeyId = os.environ.get('accessKey')
+SecretKey = os.environ.get('secretKey')
+
+def signature(pre_signed_text: 'str'):
   hash_code = hmac.new(SecretKey.encode(), pre_signed_text.encode(), hashlib.sha256).digest()
   signature = urlencode({'Signature': base64.b64encode(hash_code).decode()})
-  url = 'https://' + base_uri + endpoint + '?' + params + '&' + signature
-  return url
-
+  return signature
 
 def swap_openorders(contract_code: 'str'):
     body = {"contract_code":contract_code}
     method = 'POST'
     endpoint = '/swap-api/v1/swap_openorders'
-    url = build_url(method, endpoint)
+    timestamp = str(datetime.utcnow().isoformat())[0:19]
+    params = urlencode({'AccessKeyId': AccessKeyId,
+                    'SignatureMethod': 'HmacSHA256',
+                    'SignatureVersion': '2',
+                    'Timestamp': timestamp
+                   })
+    pre_signed_text = method + '\n' + base_uri + '\n' + endpoint + '\n' + params
+    url = 'https://' + base_uri + endpoint + '?' + params + '&' + signature(pre_signed_text)
     response = requests.request(method, url, json = body)
     swap_openorders = json.loads(response.text)
     print(swap_openorders)
+
+
+def swap_mark_price_kline(contract_code: 'str', period: 'str', size: 'str'):
+    body = {"contract_code":contract_code}
+    method = 'POST'
+    endpoint = '/swap-api/v1/swap_mark_price_kline'
+    url = build_url(method, endpoint)
+    response = requests.request(method, url)
+    swap_openorders = json.loads(response.text)
+    print(swap_openorders)
+
+
