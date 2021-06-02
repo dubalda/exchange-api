1572ee92e15a318b735455a96541992cd42c93eb
Laptev Stanislav <laptevss@gmail.com>
2021-06-02 15:49:55 +0300

    huobi python init

--- huobi/python/install.sh
+++ huobi/python/install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 yum install -y python3-pip gcc python3-devel
-pip3 install git+https://github.com/hbdmapi/huobi_futures_Python.git
--- huobi/python/tests/config.py
+++
@@ -1,5 +0,0 @@
-config = {
-    "access_key":"x-x-x-x",
-    "secret_key":"x-x-x-x",
-    "host":"api.hbdm.com"
-}
