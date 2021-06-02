5b80c18b437e3ec4abb71c43fbc059c5b7b12a73
Laptev Stanislav <laptevss@gmail.com>
2021-06-02 18:36:38 +0300

    huobi python init

--- huobi/python/create_order.py
+++ huobi/python/create_order.py
@@ -1,18 +1,3 @@
-import os
-from datetime import datetime
-from urllib.parse import urlencode
-
-#Get all Accounts of the Current User
-AccessKeyId = os.environ.get('accessKey')
-SecretKey = os.environ.get('secretKey')
-timestamp = str(datetime.utcnow().isoformat())[0:19]
-params = urlencode({'AccessKeyId': AccessKeyId,
-                    'SignatureMethod': 'HmacSHA256',
-                    'SignatureVersion': '2',
-                    'Timestamp': timestamp
-                   })
-base_uri = 'api.hbdm.vn'
-
 import huobi
 
 huobi.swap_openorders('btc-usd')
--- huobi/python/huobi/__init__.py
+++ huobi/python/huobi/__init__.py
@@ -4,8 +4,30 @@ import hmac
 import hashlib
 import base64
 from urllib.parse import urlencode
+from datetime import datetime
+import os
+
+global AccessKeyId
+global SecretKey
+global timestamp
+global params
+global base_uri
+
+def init():
+  #Get all Accounts of the Current User
+  AccessKeyId = os.environ.get('accessKey')
+  SecretKey = os.environ.get('secretKey')
+  timestamp = str(datetime.utcnow().isoformat())[0:19]
+  params = urlencode({'AccessKeyId': AccessKeyId,
+                    'SignatureMethod': 'HmacSHA256',
+                    'SignatureVersion': '2',
+                    'Timestamp': timestamp
+                   })
+  base_uri = 'api.hbdm.vn'
+
 
 def swap_openorders(contract_code: 'str'):
+    init()
     body = {"contract_code":contract_code   }
     method = 'POST'
     endpoint = '/swap-api/v1/swap_openorders'
