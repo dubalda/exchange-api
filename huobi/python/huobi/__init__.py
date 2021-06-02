b565bec30c014451b9ec3f4ae73c393b7e13cebd
Laptev Stanislav <laptevss@gmail.com>
2021-06-02 18:57:26 +0300

    huobi python init

--- huobi/python/huobi/__init__.py
+++ huobi/python/huobi/__init__.py
@@ -7,7 +7,7 @@ from urllib.parse import urlencode
 from datetime import datetime
 import os
 
-def init():
+def build_url(method: 'str', endpoint: 'str'):
   global AccessKeyId
   global SecretKey
   global timestamp
@@ -24,17 +24,18 @@ def init():
                     'Timestamp': timestamp
                    })
   base_uri = 'api.hbdm.vn'
+  pre_signed_text = method + '\n' + base_uri + '\n' + endpoint + '\n' + params
+  hash_code = hmac.new(SecretKey.encode(), pre_signed_text.encode(), hashlib.sha256).digest()
+  signature = urlencode({'Signature': base64.b64encode(hash_code).decode()})
+  url = 'https://' + base_uri + endpoint + '?' + params + '&' + signature
+  return url
 
 
 def swap_openorders(contract_code: 'str'):
-    init()
     body = {"contract_code":contract_code}
     method = 'POST'
     endpoint = '/swap-api/v1/swap_openorders'
-    pre_signed_text = method + '\n' + base_uri + '\n' + endpoint + '\n' + params
-    hash_code = hmac.new(SecretKey.encode(), pre_signed_text.encode(), hashlib.sha256).digest()
-    signature = urlencode({'Signature': base64.b64encode(hash_code).decode()})
-    url = 'https://' + base_uri + endpoint + '?' + params + '&' + signature
+    url = build_url(method, endpoint)
     response = requests.request(method, url, json = body)
     swap_openorders = json.loads(response.text)
     print(swap_openorders)
